76a460877c2d6fff557cf145aab3a1ecc0dbe13a
Victorola <victoluolatunji@gmail.com>
2024-11-11 01:48:03 +0100

    .

---
+++ src/components/home/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="px-[100.14px] pt-[99px] pb-[130px] font-dm bg-black/30 rounded-[11.308px] flex flex-col items-center justify-center gap-[35px]">
+      <div></div>
+    </section>
+  );
+}
